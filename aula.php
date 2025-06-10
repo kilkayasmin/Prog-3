e5092c8ede47f605ef272fed4bf99e4025a7df2e
kilkayasmin <yasmin.2023305779@aluno.iffar.edu.br>
2025-06-10 08:27:08 -0300

    Aula.php - 29-04

--- aula.php
+++ aula.php
@@ -1,37 +1,52 @@
-<?php 
+<?php
+// Definindo a classe caneta
 
-class produto{
-    public $nome;
-    public $preco;
-    public $quantidade;
+class Caneta{
+    // definir atributos
+    public $cor;
+    public $marca;
+    public $ponta;
+    public $tamanho;
+    public $carga;
 
-    public function exibirInformacoes() {
-        echo "Nome: {$this-> nome}, Preço: {$this-> preco}, Quantidade:  {$this-> quantidade} . <br>";
+    // definir os metodos
+    public function escrever(){
+        echo"escrevendo...<br>";
+    }    
+    public function rabiscar(){
+        echo "rabiscando...<br>";
+    }
+    public function sublinhar(){
+        echo "sublinhando...<br>";
+    }
+    public function pintar(){
+        echo "pintando...<br>";
     }
 
 }
 
-      $produto1 = new produto();
-    $produto1->nome = "Caneta"; 
-    $produto1->preco = " 2.5"; 
-    $produto1->quantidade = " 10"; 
-    $produto1->exibirInformacoes();
+// Instanciando a classa caneta
+$caneta1 = new Caneta();
+// como os atributos da classe caneta são publicos, podemos acessá-los diretamentes
+$caneta1->cor = "azul";
+$caneta1->marca = "BIC";
+$caneta1->ponta = 0.5;
+$caneta1->tamanho = "grande";
+$caneta1->carga = 100;
 
-      $produto2 = new produto();
-    $produto2->nome = " Caderno";
-    $produto2->preco = " 9.5";
-    $produto2->quantidade = " 15";
-    $produto2->exibirInformacoes();
+// Exibindo os atributos da caneta
+echo "Cor: " . $caneta1->cor ."<br>";
+echo "Marca: " . $caneta1->marca ."<br>";
+echo "Ponta: " . $caneta1->ponta ."<br>";
+echo "Tamanho: ". $caneta1->tamanho ."<br>";
+echo "carga: ". $caneta1->carga ."<br>";
 
-    function mostrarMediaPrecos($produto1, $produto2)
-    {
-        $media = ($produto1 -> preco + $produto2 -> preco) /2;
-        echo "A média dos preços dos produtos é: R$ " . number_format($media, 2, ',', '.') . "<br>"; 
-    }
-    
-    mostrarMediaPrecos($produto1, $produto2);
+// chamar o métodos da caneta
+$caneta1->escrever();
+$caneta1->rabiscar();
+$caneta1->sublinhar();
+$caneta1->pintar();
 
-    
-    
+
 
 ?>
